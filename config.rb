f34b1dc2c782dcf7bcb2bcb8c3f1fbbf133670ea
David Minnerly <dtreeLive@hotmail.com>
2014-11-13 11:55:49 -0500

    Reimplement Sass (I couldn't stay away)

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+require 'susy'
+
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "sass"
+images_dir      = "img"
+javascripts_dir = "js"
+
+line_comments = false
